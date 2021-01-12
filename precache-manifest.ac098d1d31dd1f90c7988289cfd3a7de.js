b2d4dd671a9ca870ef033746dda11024e776b1b5
Jaseel P V <jaseelbavu@gmail.com>
2021-01-12 11:36:24 +0400

    gh-pages

---
+++ precache-manifest.ac098d1d31dd1f90c7988289cfd3a7de.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "23d9eae12453c8e894ceaa51ade22639",
+    "url": "/img/theme/jb.jpg"
+  },
+  {
+    "revision": "9c0f5c639edb57ef131ade3487c0d3f7",
+    "url": "/pdf/JASEELPVRESUME.pdf"
+  },
+  {
+    "revision": "f803f8fc908c6d64e5f9",
+    "url": "/js/chunk-vendors.2ce00bec.js"
+  },
+  {
+    "revision": "7fa8378e5de4f96b8227315409affee5",
+    "url": "/img/theme/school-logo.png"
+  },
+  {
+    "revision": "cf82b8acbf532e87bca5c9d9a37d964e",
+    "url": "/img/theme/pms-logo.jpg"
+  },
+  {
+    "revision": "d7b37a023dd41d64015939b0c58d73c3",
+    "url": "/img/theme/oxford-logo.jpeg"
+  },
+  {
+    "revision": "b480cbad755e9d1de14ded235f501ef5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6d8de6723834a157142",
+    "url": "/js/app.435afa61.js"
+  },
+  {
+    "revision": "665cbcb8e583598076345d209d3e96b9",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "5f11d98f94e419959e4bf801a7358f58",
+    "url": "/img/theme/examsummary-logo.png"
+  },
+  {
+    "revision": "f38577a0f8e5abdd413a4271e08b5da5",
+    "url": "/img/theme/assabah-logo.png"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f803f8fc908c6d64e5f9",
+    "url": "/css/chunk-vendors.e935173d.css"
+  },
+  {
+    "revision": "b6d8de6723834a157142",
+    "url": "/css/app.ebbb32cc.css"
+  },
+  {
+    "revision": "98eada64edcaaca90a1b0e2f65beb2d7",
+    "url": "/9554e601b54436dfd619.worker.js"
+  },
+  {
+    "revision": "90a94b0132caa36619d0cb143729a46b",
+    "url": "/404.html"
+  }
+];
